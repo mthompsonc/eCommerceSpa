1c8de03d7bfe3f367bc5a12f4b0e711bea9fd47d
María Paz Thompson <m.paz.thompson@gmail.com>
2018-02-24 18:45:24 -0300

    agregando data

---
+++ assets/js/data.js
@@ -0,0 +1,184 @@
+"notebooks": [
+  {
+  "img": "https://image.ibb.co/irDcdH/cuaderno_take_flight.jpg",
+  "name": "Cuaderno Anillado Globo Aerostático",
+  "price": "$8.990",
+  "description": "Déjate inspirar por este increíble cuaderno de Peter Pauper Press. Incluye 160 páginas levemente rayadas para escribir reflexiones personales, realizar sketches o simplemente anotar frases. Las hojas cuentan con calidad de archivo libre de ácidos que destaca la escritura. Se trata de un cuaderno práctico, duradero y personalizable. Cuenta con cubiertas que sirven de pizarrón, banda elástica para marcar páginas o mantenerlo cerrado y encuadernación chata con espiral. Medidas: 20.95 x 15.87 x 1.47 cm.",
+  "category": "notebooks",
+  "code": "PPA-2135",
+  "stock": "5"
+  },
+   {
+  "img": "https://image.ibb.co/izLhdH/58ea5aa105d04cc18d9bc8ee0ab33081.jpg",
+  "name": "Cuaderno My unicorn ate my homework",
+  "price": "$ 3.500",
+  "description": "Haz tus investigaciones en este maravilloso cuaderno. Incluye 100 páginas de composición, para que puedas escribir lo que quieras. ¡Llévalo a todas partes!. Medidas: 20 x 15.6 x 1.2 cm.",
+  "category": "notebooks",
+  "code": "GYA-3476",
+  "stock": "10"
+  },
+  {
+  "img": "https://image.ibb.co/gpcaXc/cuadernos_memory_planner_american_crafts.jpg",
+  "name": "Cuadernos American Crafts Memory Planner",
+  "price": "$ 3.500",
+  "description": "Mini cuadernos para anotar recordatorios, fechas importantes y todo tipo de notas. Forman parte de la nueva colección de American Crafts Memory Planner. El kit incluye dos cuadernos de 9 x 10 cm.",
+  "category": "notebooks, planner",
+  "code": "AMC-375039",
+  "stock": "10"
+  },
+  {
+  "img": "https://image.ibb.co/k1BoJH/b5c6d064d0d99c663d45ec71f4008654.jpg",
+  "name": "Cuaderno Psyco, but cute.",
+  "price": "$ 3.000",
+  "description": "Este cuaderno se caracteriza por su buen diseño y facilidad de transportar, ya que tiene un tamaño adecuado para cartera o mochila. Medidas: 18 x 10 x 1 cm.",
+  "category": "Notebooks",
+  "code": "CUB-1274",
+  "stock": "6"
+  },
+   {
+  "img": "https://image.ibb.co/cnRkXc/block_chico_fun_stuff.jpg",
+  "name": "Block Chico The Fun Stuff",
+  "price": "$ 3.290",
+  "description": "Un bloc imprescindible para llevar contigo a donde vayas gracias a su formato bolsillo. Podrás anotar ideas y listas de último momento. Cuenta con 75 páginas a todo color, cierre magnético, increíble diseño de Graphique de France y detalles en color oro. Medidas: 7.62 x 10 cm.",
+  "category": "notebooks, block",
+  "code": "GFR-PN1282",
+  "stock": "2"
+  },
+  {
+  "img": "https://image.ibb.co/hjJtJH/1b3b238e93dac52e185f7b479373ce35.jpg",
+  "name": "Libreta diseño por Mauro Gómez",
+  "price": "$ 5.900",
+  "description": "Libreta diseñada por Mauro Gómez, con una encuadernación rústica con un lomo forrado, este cuaderno es práctico para anotar todo tipo de cosas. Medidas: 18 x 15 x 2 cm.",
+  "category": "notebooks",
+  "code": "DCA-1253",
+  "stock": "12"
+  },
+  {
+  "img": "https://image.ibb.co/chNdJH/block_chico_magnetico_fucsia_life_moves.jpg",
+  "name": "Block Chico Magnético Fucsia Life Moves",
+  "price": "$ 3.990",
+  "description": "Un block formato cartera o bolsillo para escribir lo que quieras. Desde tus ideas y pensamientos hasta una lista de compras; sirve para todo. Block tamaño bolsillo cierre magnético, hojas croquis. Traído de USA. Medidas: 76 x 100 x 10 mm, 70 gr.",
+  "category": "notebooks, block",
+  "code": "GFR-PN1241",
+  "stock": "5"
+  },
+  {
+  "img": "https://image.ibb.co/m2NZsc/7a45ada361f3097b7e10e429d949f88c.jpg",
+  "name": "Pack 3 Cuadernos Ilustrados",
+  "price": "$ 7.990",
+  "description": "3 Cuadernos ilustrados y cocidos a mano, por la ilustradora Nuria Díaz. Por el tamaño de estos cuadernos, son prácticos para llevarlos a todas partes, toma notas, haz listas, puedes hacer sketch y serán un gran aporte en tu vida diaria.",
+  "category": "notebooks",
+  "code": "AGD-9623",
+  "stock": "notebooks, ilustraciones"
+  },
+  {
+  "img": "https://image.ibb.co/f6EVXc/libreta_flores_campestres.jpg",
+  "name": "Libreta Fuchsia Blooms",
+  "price": "$ 6.990",
+  "description": "Espectacular libreta de la colección Peter Pauper Press. Incluye 160 páginas levemente rayadas, cierre elástico en color celeste, tapa dura con diseño de flores, bolsillo interior para guardar notas y detalles en folia plateada. Cuenta con papel con calidad archivo y certificación libre de ácidos. Medidas: 12.7 x 17.78 x 1.27 cm ",
+  "category": "notebooks",
+  "code": "PPA-5595",
+  "stock": "15"
+  },
+  {
+  "img": "https://image.ibb.co/ieZ5Xc/d7b066da9ba6d9933a097c0d0dedaf08.jpg",
+  "name": "Cuaderno Flamencos",
+  "price": "$ 3.990",
+  "description": "Cuaderno con una encuadernación rústica, para que anotes lo que quieras. Medidas 18 x 15.6 x 1 cm.",
+  "category": "notebooks",
+  "code": "ADG-1346",
+  "stock": "19"
+  }
+],
+"pencils": [
+  {
+  "img": "https://image.ibb.co/hn57vx/set_crayones_acuarela.jpg",
+  "name": "Set de Crayones Acuarela",
+  "price": "$ 8.990",
+  "description": "Crea efectos increíbles con estos crayones acuarelables de Peter Pauper Press. Para conseguir un efecto suave y esfumado puedes pintar sobre papel humedecido o pasar un pincel con agua sobre tu dibujo. El set incluye doce crayones de calidad profesional solubles en agua. Son suaves y fáciles de mezclar. Medidas: 16.51 x 13.97 x 1.27 cm ",
+  "category": "pencils, watercolor",
+  "code": "PPA-0537",
+  "stock": "20"
+  },
+  {
+  "img": "https://image.ibb.co/co798H/boligrafo_pompon.jpg",
+  "name": "Bolígrafo Pom Pom Rosado",
+  "price": "$ 2.690",
+  "description": "Espectacular bolígrafo con un gran pompón de la colección Girl Gang de Blueprint. Cuenta con un llamativo diseño estilo funky con mucho color y una divertida frase ",
+  "category": "pencils",
+  "code": "BLU-SNGG1692",
+  "stock": "10"
+  },
+    {
+  "img": "https://image.ibb.co/gJsLFx/set_resaltadores.jpg",
+  "name": "Set Destacadores Varios Colores",
+  "price": "$ 7.990",
+  "description": "Set de seis resaltadores de la colección Studio Series de Peter Pauper Press. Su fórmula única permite que no se corran ni manchen incluso en papel fino. Incluye seis colores que representan temas comunes en el estudio de la Biblia (azul, morado, verde, rosa, naranja y amarillo). Medidas: 7.97 x 14.60 x 1.27 cm.",
+  "category": "pencils",
+  "code": "PPA-4733",
+  "stock": "3"
+  },
+  {
+  "img": "https://image.ibb.co/k57WNc/estuche_acuarela.jpg",
+  "name": "Estuche Acuarela Creative Devotion",
+  "price": "$ 6.290",
+  "description": "Un estuche perfecto para proteger tus lápices, stickers, stencils y todo tipo de útiles. Cuenta con un diseño espectacular de American Crafts con cierre. Medidas: 20.32 x 10.16 cm.",
+  "category": "pencils, watercolor",
+  "code": "AMC-343057",
+  "stock": "4"
+  },
+  {
+  "img": "https://image.ibb.co/k0paFx/estuche_flores_negras.jpg",
+  "name": "Estuche Rosas Negras Creative Devotion",
+  "price": "$ 5.990",
+  "description": "Un estuche perfecto para proteger tus lápices, stickers, stencils y todo tipo de útiles. Cuenta con un diseño espectacular de American Crafts con cierre. Medidas: 20.32 x 10.16 cm.",
+  "category": "3",
+  "code": "AMC-342606",
+  "stock": "3"
+  },
+  {
+  "img": "https://image.ibb.co/hw1GNc/boligrafos_pasteles_creative_devotion.jpg",
+  "name": "Bolígrafos Pasteles Creative Devotion",
+  "price": "$ 3.990",
+  "description": "Estos bolígrafos en colores pasteles son perfectos para escribir notas. Pertenecen a la colección Creative Devotion de American Crafts. Cuentan con tinta negra y una punta de bola ideal para escribir en diarios íntimos o biblias. Medidas: 13.97 cm (largo).",
+  "category": "",
+  "code": "AMC-342592",
+  "stock": "3"
+  },
+  {
+  "img": "",
+  "name": "Plumas para Lettering Tinta Negra",
+  "price": "$ 3.290",
+  "description": "Una pluma de la colección Creative Devotion de American Crafts especial para lettering. Es perfecta para escribir y diseñar sobre papel fino estilo Biblia. Producto no tóxico, libre de ácidos.",
+  "category": "pencils, letteting",
+  "code": "AMC-342593",
+  "stock": "6"
+  },
+  {
+  "img": "",
+  "name": "",
+  "price": "",
+  "description": "",
+  "category": "",
+  "code": "",
+  "stock": ""
+  },
+  {
+  "img": "",
+  "name": "",
+  "price": "",
+  "description": "",
+  "category": "",
+  "code": "",
+  "stock": ""
+  },
+  {
+  "img": "",
+  "name": "",
+  "price": "",
+  "description": "",
+  "category": "",
+  "code": "",
+  "stock": ""
+  },
+],
