9fb46ce5c6e400de147ccaa8f849bb47d0929546
María Paz Thompson <m.paz.thompson@gmail.com>
2018-02-26 17:32:51 -0300

    ordenando archivos, aplicando eslint, etc

--- javascript/json_store.js
+++ javascript/json_store.js
@@ -1,5 +1,4 @@
 (function($) {
-
   var app = $.sammy('#main', function() {
     this.use('Template');
     this.use('Session');
@@ -7,38 +6,43 @@
     this.around(function(callback) {
       var context = this;
       this.load('data/data.json')
-          .then(function(items) {
-            context.items = items;
-          })
-          .then(callback);
+        .then(function(items) {
+          context.items = items;
+        })
+        .then(callback);
     });
 
     this.get('#/', function(context) {
       context.app.swap('');
       $.each(this.items, function(i, item) {
-        context.render('templates/item.template', {id: i, item: item})
-               .appendTo(context.$element());
+        context.render('templates/item.template', {
+          id: i,
+          item: item
+        })
+          .appendTo(context.$element());
       });
     });
 
     this.get('#/item/:id', function(context) {
       this.item = this.items[this.params['id']];
-      if (!this.item) { return this.notFound(); }
+      if (!this.item) {
+        return this.notFound();
+      }
       this.partial('templates/item_detail.template');
     });
 
     this.post('#/cart', function(context) {
-      var item_id = this.params['item_id'];
+      var itemId = this.params['item_id'];
       // fetch the current cart
       var cart = this.session('cart', function() {
         return {};
       });
-      if (!cart[item_id]) {
+      if (!cart[itemId]) {
         // this item is not yet in our cart
         // initialize its quantity with 0
-        cart[item_id] = 0;
+        cart[itemId] = 0;
       }
-      cart[item_id] += parseInt(this.params['quantity'], 10);
+      cart[itemId] += parseInt(this.params['quantity'], 10);
       // store the cart
       this.session('cart', cart);
       this.trigger('update-cart');
@@ -50,20 +54,17 @@
         sum += quantity;
       });
       $('.cart-info')
-          .find('.cart-items').text(sum).end()
-          .animate({paddingTop: '30px'})
-          .animate({paddingTop: '10px'});
+        .find('.cart-items').text(sum).end()
+        .animate({paddingTop: '30px'})
+        .animate({paddingTop: '10px'});
     });
 
     this.bind('run', function() {
       // initialize the cart display
       this.trigger('update-cart');
     });
-
   });
-
   $(function() {
     app.run('#/');
   });
-
 })(jQuery);
